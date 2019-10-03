dbfbdf6ca8b0fbefe0487f1ea7dd2bd83a0417b6
vmartinez <vmartinez@latinid.com.mx>
2019-10-03 15:52:45 -0500

    Agregar archivos de proyecto.

---
+++ Buscaminas.Controller/Aleatorio.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buscaminas.Controller
+{
+    public class Aleatorio
+    {
+        public static List<int> Get()
+        {
+            try
+            {
+                var lst = new List<int>();
+                return lst;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
